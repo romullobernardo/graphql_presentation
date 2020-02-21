224242deabb915015de00213142e8bca6033d3c6
Romullo Bernardo <romullobernardo@XRT25-22B.local>
2020-02-21 13:12:09 -0500

    first commit

---
+++ api/src/models/Card.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const cardSchema = new Schema(
+{
+  description: { type: String, required: true },
+},
+{
+  timestamps: true // Create timestamp
+})
+  
+export default mongoose.model('Card', cardSchema)
+  
